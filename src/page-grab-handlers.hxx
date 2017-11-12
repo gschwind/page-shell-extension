0a289c8ceb7c75ae2d1062e5d0739c59697dd6b0
Benoit Gschwind <gschwind@gnu-log.net>
2017-11-12 14:20:08 +0100

    remove resize_mode_e

--- src/page-grab-handlers.hxx
+++ src/page-grab-handlers.hxx
@@ -105,36 +105,6 @@ public:
 	using grab_handler_t::key_release;
 };
 
-struct mode_data_notebook_client_menu_t  : public grab_default_t {
-	weak_ptr<notebook_t> from;
-	weak_ptr<client_managed_t> client;
-	bool active_grab;
-	rect b;
-
-	mode_data_notebook_client_menu_t(page_t * ctx) : grab_default_t{ctx} {
-		reset();
-	}
-
-	void reset() {
-		from.reset();
-		client.reset();
-		active_grab = false;
-	}
-
-};
-
-enum resize_mode_e {
-	RESIZE_NONE,
-	RESIZE_TOP_LEFT,
-	RESIZE_TOP,
-	RESIZE_TOP_RIGHT,
-	RESIZE_LEFT,
-	RESIZE_RIGHT,
-	RESIZE_BOTTOM_LEFT,
-	RESIZE_BOTTOM,
-	RESIZE_BOTTOM_RIGHT
-};
-
 }
 
 
